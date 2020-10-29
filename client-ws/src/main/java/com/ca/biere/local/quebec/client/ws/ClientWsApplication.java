260d839b309d2c4186da1c081793860c14f6d4b5
brunomathidios <brunomathidios@yahoo.com.br>
2020-10-29 18:32:40 -0400

    first commit: criação dos dois projetos back-end

---
+++ client-ws/src/main/java/com/ca/biere/local/quebec/client/ws/ClientWsApplication.java
@@ -0,0 +1,13 @@
+package com.ca.biere.local.quebec.client.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientWsApplication.class, args);
+	}
+
+}
---
+++ client-ws/src/test/java/com/ca/biere/local/quebec/client/ws/ClientWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca.biere.local.quebec.client.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
